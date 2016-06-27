2832f06495c948d01d82c0bc25bc2778710df290
Dylan Landry <dylan.landry@maine.edu>
2016-06-27 19:54:08 -0400

    Obstacles Spawning/Despawning!!
    Before I pull request I am going to thoroughly comment my code.

--- djGame/Assets/scripts/Chunk.cs
+++ djGame/Assets/scripts/Chunk.cs
@@ -21,6 +21,9 @@ public class Chunk
     stored in their respective ArrayLists here so that they can
     be conveniently unloaded.
     */
+    private ArrayList _groundObstacles = new ArrayList();
+    //Chance of a ground obstacle spawning on a unit (0f - 1f).
+    public float groundObstacleSpawnChance = .10f;
 
     //Constructor.
     public Chunk(Vector2 position, int chunkNumber)
@@ -35,12 +38,39 @@ public class Chunk
     //Any objects in the chunk should be spawned in Load().
     private void Load()
     {
+
+        //A for loop with an index representing each unit within the chunk.
+        Vector2 indexPosition = new Vector2(position.x - chunkWidth / 2, 0);
+        for (int i = 0; i < chunkWidth; i++)
+        {
+            indexPosition.x++;
+
+            //           
+            if (Mathf.Round(Random.Range(0, chunkWidth)) <= (chunkWidth * groundObstacleSpawnChance))
+            {
+                GameObject groundObstacle = Object.Instantiate(Resources.Load("Ground_Obstacle"), 
+                    indexPosition, Quaternion.Euler(0, 0, 0)) as GameObject;
+                groundObstacle.transform.position = new Vector2(groundObstacle.transform.position.x, 
+                    groundObstacle.transform.position.y + groundObstacle.GetComponent<Renderer>().bounds.size.y/2);
+                _groundObstacles.Add(groundObstacle);
+            }
+
+            
+        }
+
         Debug.Log("Chunk #" + chunkNumber + " loaded.");
     }
 
     //Any objects loaded in this chunk should be despawned in Unload().
     public void Unload()
     {
+        for (int i = 0; i < _groundObstacles.Count; i++)
+        {
+            Object.Destroy((GameObject)_groundObstacles[i]);
+            _groundObstacles.Remove(((GameObject)_groundObstacles[i]));
+            i--;
+        }
+
         Debug.Log("Chunk #" + chunkNumber + " unloaded.");
     }
 
