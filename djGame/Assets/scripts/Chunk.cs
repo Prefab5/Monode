cb9e3318843e2e70d005a9f841ad65a21a360220
Dylan Landry <dylan.landry@maine.edu>
2016-06-27 15:08:19 -0400

    Chunk gameObject Load/Unload Methods Added
    Added some methods and comments in the Chunk class that hopefully helps
    us understand how to use chunking to manage gameObjects.

--- djGame/Assets/scripts/Chunk.cs
+++ djGame/Assets/scripts/Chunk.cs
@@ -1,27 +1,47 @@
 ﻿/*
 Description: A chunk generates a multitude of game objects within it's bounds
 in which the player moves through. All prefabs shoulds be generated here.
+Created: 6/26/16
 */
 
 using UnityEngine;
 using System.Collections;
 
-public class Chunk {
-    public const int chunkWidth = 24;
+public class Chunk
+{
+    //Width of a chunk in Unity units. A larger chunk will load more objects at once.
+    public const int chunkWidth = 28;
+    //Center position of chunk.
     public Vector2 position;
+    //Identification number of chunk.
     public int chunkNumber;
 
-	public Chunk(Vector2 position, int chunkNumber)
+    /*
+    gameObject ArrayLists. Any gameObjects of the same type should be 
+    stored in their respective ArrayLists here so that they can
+    be conveniently unloaded.
+    */
+
+    //Constructor.
+    public Chunk(Vector2 position, int chunkNumber)
     {
         this.position = position;
         this.chunkNumber = chunkNumber;
-        Debug.Log("Chunk #" + chunkNumber + " spawned. Position:(" + position.x + ", " + position.y + ")");
+
+        Load();
 
     }
 
-    public void Remove()
+    //Any objects in the chunk should be spawned in Load().
+    private void Load()
     {
-        Debug.Log("Chunk #" + chunkNumber + " deleted.");
+        Debug.Log("Chunk #" + chunkNumber + " loaded.");
+    }
+
+    //Any objects loaded in this chunk should be despawned in Unload().
+    public void Unload()
+    {
+        Debug.Log("Chunk #" + chunkNumber + " unloaded.");
     }
 
 }
--- djGame/Assets/scripts/ChunkController.cs
+++ djGame/Assets/scripts/ChunkController.cs
@@ -12,16 +12,17 @@ public class ChunkController : MonoBehaviour
 
     private static Vector2 _initialCameraPosition;
     private Vector2 _currentCameraPosition;
-    private static int _currentChunk;
+    private static int _chunkNumber;
     private ArrayList Chunks = new ArrayList();
+    public bool debug = false;
 
 
     void Start()
     {
         _initialCameraPosition = Camera.main.transform.position;
-        _currentChunk = 0;
-        _currentChunk++;
-        Chunks.Add(new Chunk(_initialCameraPosition + new Vector2(Chunk.chunkWidth / 4, 0), _currentChunk));
+        _chunkNumber = 1;
+        //Creates first chunk where the camera is.
+        Chunks.Add(new Chunk(_initialCameraPosition + new Vector2(Chunk.chunkWidth / 4, 0), _chunkNumber));
 
     }
 
@@ -30,25 +31,32 @@ public class ChunkController : MonoBehaviour
     {
         _currentCameraPosition = Camera.main.transform.position;
 
-        //Create chunk.      
+        //If there is only one chunk right now and we soon need another, make another.    
         if (Chunks.Count == 1)
         {
             if (_currentCameraPosition.x >= ((Chunk)Chunks[0]).position.x + Chunk.chunkWidth / 4)
             {
-                _currentChunk++;
-                Chunks.Add(new Chunk(((Chunk)Chunks[0]).position + new Vector2(Chunk.chunkWidth, 0), _currentChunk));
+                _chunkNumber++;
+                Chunks.Add(new Chunk(((Chunk)Chunks[0]).position + new Vector2(Chunk.chunkWidth, 0), _chunkNumber));
             }
 
         }
+
+        /*
+        If we have more than one chunk,
+        check each chunk to see if its far enough left to be deleted.
+        */
         else
         {
             for (int i = 0; i < Chunks.Count; i++)
             {
                 {
-
                     if (((Chunk)Chunks[i]).position.x + Chunk.chunkWidth / 2 <= _currentCameraPosition.x - Chunk.chunkWidth / 4)
                     {
-                        ((Chunk)Chunks[i]).Remove();
+                        //Call Chunk method that removes previously loaded gameObjects.
+                        ((Chunk)Chunks[i]).Unload();
+
+                        //Remove that Chunk object from our ArrayList.
                         Chunks.Remove((Chunk)Chunks[i]);
                         break;
                     }
@@ -56,7 +64,16 @@ public class ChunkController : MonoBehaviour
             }
         }
 
-        //Delete chunk.
+        //Draws boundaries of chunks.
+        if (debug)
+        {
+            foreach (Chunk chunk in Chunks){
+                Debug.DrawLine(new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
+                Debug.DrawLine(new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
+                Debug.DrawLine(new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Color(1, 0, 0));
+                Debug.DrawLine(new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
+            }
+        }
 
     }
 }
