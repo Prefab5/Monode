031994d6b9d0de5a46a412dc728d93acececc9e9
Dylan Landry <dylan.landry@maine.edu>
2016-06-30 15:58:17 -0400

    Working Camera & Chunk Movement & Player Script
    The chunks move while the camera stays put. A method fires on the Player
    and ChunkController when the player hits a gameObject with the tag
    "obstacle_collider". Had to make a PlayerController to host the method.

--- djGame/Assets/scripts/ChunkController.cs
+++ djGame/Assets/scripts/ChunkController.cs
@@ -11,67 +11,102 @@ public class ChunkController : MonoBehaviour
 
     private static Vector2 _initialCameraPosition;
     private Vector2 _currentCameraPosition;
-    private static int _chunkNumber;
-    private ArrayList _chunks = new ArrayList();
+    private static int _chunkCount;
     public bool debug = true;
-
+    public float chunkMoveSpeed = 4f;
 
     void Start()
     {
         _initialCameraPosition = Camera.main.transform.position;
-        _chunkNumber = 1;
+        _chunkCount = 1;
+
         //Creates first chunk where the camera is.
-        _chunks.Add(new Chunk(_initialCameraPosition + new Vector2(Chunk.chunkWidth / 4, 0), _chunkNumber));
+        GameObject chunk = Object.Instantiate(Resources.Load("Chunk"),
+                    _initialCameraPosition + new Vector2(Chunk.chunkWidth / 4, 0), 
+                    Quaternion.Euler(0, 0, 0)) as GameObject;
+
+        //Set parent for hierachy orginizational purposes.
+        chunk.transform.parent = transform;
+        chunk.transform.name = "Chunk " + _chunkCount;
+        
+        _chunkCount++;
+
 
     }
 
 
     void Update()
     {
+        ChunkSpawnManagement();
+        ChunkMovement();
+        ChunkDebugRect();
+    }
+
+    void ChunkSpawnManagement()
+    {
         _currentCameraPosition = Camera.main.transform.position;
 
         //If there is only one chunk right now and we soon need another, make another.    
-        if (_chunks.Count == 1)
+        if (gameObject.transform.childCount == 1)
         {
-            if (_currentCameraPosition.x >= ((Chunk)_chunks[0]).position.x + Chunk.chunkWidth / 4)
+            if (_currentCameraPosition.x >= gameObject.transform.GetChild(0).position.x + Chunk.chunkWidth / 4)
             {
-                _chunkNumber++;
-                _chunks.Add(new Chunk(((Chunk)_chunks[0]).position + new Vector2(Chunk.chunkWidth, 0), _chunkNumber));
+               
+                
+                GameObject chunk = Object.Instantiate(Resources.Load("Chunk"),
+                    gameObject.transform.GetChild(0).position + new Vector3(Chunk.chunkWidth, 0, 0), Quaternion.Euler(0, 0, 0)) as GameObject;
+
+                chunk.transform.parent = transform;
+                chunk.transform.name = "Chunk " + _chunkCount;
+                _chunkCount++;
+
             }
 
         }
 
-        /*If we have more than one chunk,
-        check each chunk to see if its far enough left to be deleted.
-        */
+        /*If we have more than one chunk,check each chunk to see if its 
+        far enough left to be deleted.*/
         else
         {
-            for (int i = 0; i < _chunks.Count; i++)
+            for (int i = 0; i < gameObject.transform.childCount; i++)
             {
                 {
-                    if (((Chunk)_chunks[i]).position.x + Chunk.chunkWidth / 2 <= _currentCameraPosition.x - Chunk.chunkWidth / 4)
+                    if (gameObject.transform.GetChild(i).position.x + Chunk.chunkWidth / 2 <= _currentCameraPosition.x - Chunk.chunkWidth / 4)
                     {
-                        //Call Chunk method that removes previously loaded gameObjects.
-                        ((Chunk)_chunks[i]).Unload();
 
-                        //Remove that Chunk object from our ArrayList.
-                        _chunks.Remove((Chunk)_chunks[i]);
+                        GameObject.Destroy(gameObject.transform.GetChild(i).gameObject);
                         break;
                     }
                 }
             }
         }
+    }
 
-        //Draws boundaries of _chunks.
+    void ChunkDebugRect()
+    {
         if (debug)
         {
-            foreach (Chunk chunk in _chunks){
-                Debug.DrawLine(new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
-                Debug.DrawLine(new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
-                Debug.DrawLine(new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y + Chunk.chunkWidth / 2), new Color(1, 0, 0));
-                Debug.DrawLine(new Vector2(chunk.position.x - Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Vector2(chunk.position.x + Chunk.chunkWidth / 2, chunk.position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
+            for(int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                Debug.DrawLine(new Vector2(gameObject.transform.GetChild(i).position.x - Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y + Chunk.chunkWidth / 2), new Vector2(gameObject.transform.GetChild(i).position.x - Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
+                Debug.DrawLine(new Vector2(gameObject.transform.GetChild(i).position.x + Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y + Chunk.chunkWidth / 2), new Vector2(gameObject.transform.GetChild(i).position.x + Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
+                Debug.DrawLine(new Vector2(gameObject.transform.GetChild(i).position.x - Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y + Chunk.chunkWidth / 2), new Vector2(gameObject.transform.GetChild(i).position.x + Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y + Chunk.chunkWidth / 2), new Color(1, 0, 0));
+                Debug.DrawLine(new Vector2(gameObject.transform.GetChild(i).position.x - Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y - Chunk.chunkWidth / 2), new Vector2(gameObject.transform.GetChild(i).position.x + Chunk.chunkWidth / 2, gameObject.transform.GetChild(i).position.y - Chunk.chunkWidth / 2), new Color(1, 0, 0));
             }
         }
+    }
+
+    void ChunkMovement()
+    {
+        for(int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).transform.Translate(new Vector2(-1 * chunkMoveSpeed * Time.deltaTime, 0));
+        }
+    }
+
+    public void PlayerCollision()
+    {
 
     }
+
 }
---
+++ djGame/Assets/scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public ChunkController chunkController;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.tag == "collision_obstacle")
+        {
+            chunkController.PlayerCollision();
+            PlayerCollision();
+        }
+    }
+
+    void PlayerCollision()
+    {
+
+    }
+
+}
