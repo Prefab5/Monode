847479c9c2809ae4aee77523a0124d20bd5cc3e8
Dylan Landry <dylan.landry@maine.edu>
2016-07-01 15:30:23 -0400

    Added Character with Gravity

--- djGame/Assets/scripts/PlayerController.cs
+++ djGame/Assets/scripts/PlayerController.cs
@@ -5,6 +5,26 @@ public class PlayerController : MonoBehaviour {
 
     public ChunkController chunkController;
 
+    public float jumpHeight = 100f;
+
+    public Transform groundPoint;
+    public float groundPointRadius;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Checks to see if player is in contact with ground directly beneath them.
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundPointRadius, groundMask);
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         
