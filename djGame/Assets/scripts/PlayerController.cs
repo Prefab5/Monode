2e28700faa4e0d9f922f9658d3ac9a0d13af0bdc
dyllandry <dylan.landry@maine.edu>
2016-07-03 13:18:32 -0400

    Incrementing Score Implemented
    The score increments per second based on a rate set by the
    PlayerController. The PlayerController requires a reference to the
    ScoreController to control setting the increment rate, pausing the
    score increment on collision with an object, resuming the score
    increment when the player begins running again, and any future controls
    such as increasing the score in bursts when something is picked up like
    a power.

--- djGame/Assets/scripts/PlayerController.cs
+++ djGame/Assets/scripts/PlayerController.cs
@@ -1,66 +1,84 @@
-﻿using UnityEngine;
+﻿/************************************************
+ * Definition: Methods for controlling the player.
+ * 
+ * Created: ??/??/??
+ * **********************************************/
+
+using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-    public ChunkController chunkController;
+	public ChunkController chunkController;
 
-    private float jumpHeight = 500f;
-    private float knockDownTime = 2f;
-    private float timeKnockedDown = 0;
-    private bool collision = false;
+	private float jumpHeight = 500f;
+	private float knockDownTime = 2f;
+	private float timeKnockedDown = 0;
+	private bool collision = false;
 
-    public Transform groundPoint;
-    public float groundPointRadius;
-    public LayerMask groundMask;
+	//For detecting grounded state to allow jumping.
+	public Transform groundPoint;
+	public float groundPointRadius;
+	public LayerMask groundMask;
+	bool isGrounded;
 
-    bool isGrounded;
-    Rigidbody2D rb2D;
+	Rigidbody2D rb2D;
 
-    void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
+	//For controlling the score.
+	public GameObject scoreObject;
+	private ScoreController score;
 
-    void Update()
-    {
-        //Checks to see if player is in contact with ground directly beneath them.
-        isGrounded = Physics2D.OverlapCircle(groundPoint.position, groundPointRadius, groundMask);
 
-        //Jumping controls.
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb2D.AddForce(new Vector2(0, jumpHeight));
+	void Start ()
+	{
+		rb2D = GetComponent<Rigidbody2D> ();
+		score = scoreObject.GetComponent<ScoreController> ();
+		score.SetScoreRate (10);
+	}
 
-        }
+	void Update ()
+	{
+		JumpControl ();
 
-        PlayerCollision();
+		if (collision) {
+			KnockDown ();
+		}
 
-    }
+	}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
+	void JumpControl ()
+	{
+		//Checks to see if player is in contact with ground directly beneath them.
+		isGrounded = Physics2D.OverlapCircle (groundPoint.position, groundPointRadius, groundMask);
+
+		//Jumping controls.
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			rb2D.AddForce (new Vector2 (0, jumpHeight));
+
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
         
-        if (other.gameObject.tag == "collision_obstacle")
-        {
-            chunkController.PlayerCollision();
-            collision = true;
-        }
-    }
+		if (other.gameObject.tag == "collision_obstacle") {
+			chunkController.PlayerCollision ();
+			collision = true;
+			score.PauseScore ();
+		}
+	}
 
-    void PlayerCollision()
-    {
-        if (collision)
-        {
-            timeKnockedDown += Time.deltaTime;
+	void KnockDown ()
+	{
+		timeKnockedDown += Time.deltaTime;
 
-            if(timeKnockedDown > knockDownTime)
-            {
-                timeKnockedDown = 0;
-                collision = false;
-                chunkController.GetComponent<ChunkController>().ResumeMovement();
-            }
-        }
-    }
+		if (timeKnockedDown > knockDownTime) {
+			timeKnockedDown = 0;
+			collision = false;
+			chunkController.GetComponent<ChunkController> ().ResumeMovement ();
+			score.ResumeScore ();
+		}
+	}
 
 }
